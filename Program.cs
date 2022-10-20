c88459a32514c6816a2cae7155cc360be1ff5c8a
Pingvu <alka04006@utb.vaxjo.se>
2022-10-20 13:59:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int siffra = 10;siffra  <31; siffra ++)
+{
+                Console.WriteLine(siffra);
+
+                for (int siffra1 = 200; siffra > 179; siffra--)
+
+                    Console.WriteLine(siffra1);
+
+                for (int siffra2 = 1000; siffra <= 1400; siffra += 50)
+
+                    Console.WriteLine(siffra2);
+
+
+
+
+            }
+        }
+    }
+}
